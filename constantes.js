0f81a76302a384d3326c41e937a270999ed09884
GioSosaGacia <giovanni-sosa-12@outlook.com>
2025-02-24 16:04:58 -0600

    Operadores

---
+++ constantes.js
@@ -0,0 +1,14 @@
+/*Constantes es una variable qye una vez definida su valor, ya no se puede cambiar
+Una constante se define en mayuscula y si tiene mas d euan palabra se separa mediente el uso de un _bajo
+const MI_CONSTANTE = 10
+*/
+const MI_CONSTANTE = 10;
+//MI_CONSTANTE = 20 NO PODEMOS cambiar el valor de una constante 
+console.log(MI_CONSTANTE);
+
+//cONSTANTE DE PI 
+console.log(Math.PI);
+
+//constante de segundos por minuto
+const SEGUNDOS_POR_MINUTOS = 60;
+console.log(SEGUNDOS_POR_MINUTOS);
